22bea1a2e98eba91398a5d9af9f9c8ad3665c985
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2016-06-24 23:35:40 +0300

    Fixed Error when camera doesn't support Infinity focus.

--- app/src/main/java/com/alekso/camcoder/MainActivity.java
+++ app/src/main/java/com/alekso/camcoder/MainActivity.java
@@ -202,7 +202,10 @@ public class MainActivity extends AppCompatActivity {
 
         // likewise for the camera object itself.
         parameters.setPreviewSize(profile.videoFrameWidth, profile.videoFrameHeight);
-        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY);
+
+        if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_INFINITY)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY);
+        }
         mCamera.setParameters(parameters);
         try {
             mCamera.setPreviewTexture(mPreview.getSurfaceTexture());
